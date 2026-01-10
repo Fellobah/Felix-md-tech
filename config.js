7804901f831c374b8d987db2092af55364656dbd
Felix Okeyo <felixelgringo052@gmail.com>
2026-01-10 11:12:09 +0300

    Update Felix bot files

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const DEFAULT_OWNER_NUMBER = '254725391914'
-
-module.exports = {
-  GOD_MODE: (process.env.GOD_MODE || 'false').toLowerCase() === 'true',
-  OWNER: process.env.OWNER || DEFAULT_OWNER_NUMBER,
-  CREATOR: process.env.CREATOR || DEFAULT_OWNER_NUMBER
-}
