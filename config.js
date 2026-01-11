f92aef4de80e0a9d4969b566a6b4c3d16050c005
Felix Okeyo <152480089+Fellobah@users.noreply.github.com>
2026-01-11 09:58:08 +0300

    DER KRIEGER
    Signed-off-by: Felix Okeyo <152480089+Fellobah@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,23 @@
+export const BOT_INFO = {
+  name: "FELIX MD TECH",
+  version: "1.0.0",
+  description: "Multi-Device WhatsApp Bot powered by Baileys",
+
+  developers: [
+    {
+      name: "WARRIOR FELIX",
+      role: "Lead Developer",
+      contact: "https://wa.me/254701881604",
+      image: "https://telegra.ph/file/896953d764d69c906a4b7.jpg"
+    },
+    {
+      name: "MCFELLO",
+      role: "Co-Developer",
+      contact: "https://wa.me/254725391914",
+      image: "https://telegra.ph/file/903e16f9effd8de11cc76.jpg"
+    }
+  ],
+
+  supportGroup: "https://chat.whatsapp.com/L2i6oDCjljt4mtBTUvTh7t",
+  license: "GPL-3.0"
+}
